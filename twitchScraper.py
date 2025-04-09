6d185ebe298300098cee0aaa4bebb66b381a42a5
Viydal <viydal04@gmail.com>
2025-04-09 12:30:15 +0930

    not scraping

--- twitchScraper.py
+++ twitchScraper.py
@@ -6,9 +6,7 @@ if __name__ == "__main__":
   time.sleep(2)
   
   endTime = time.time() + 5
-  
-  pyautogui.keyDown()
   while time.time() < endTime:
-    pyautogui.hold('a')
-  
-  pyautogui.keyUp('a')
+    pyautogui.press('a')
+    
+  pyautogui.click()
