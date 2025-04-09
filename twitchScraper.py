a903d69a6b0a0b0e750ac9010d3784a25e5a86b8
Viydal <viydal@gmail.com>
2025-04-09 19:24:40 +0930

    test

--- twitchScraper.py
+++ twitchScraper.py
@@ -1,12 +1,29 @@
-import pyautogui
+import pydirectinput
 import time
+import socket
 
-if __name__ == "__main__":
-  
-  time.sleep(2)
-  
-  endTime = time.time() + 5
-  while time.time() < endTime:
-    pyautogui.press('a')
+# Replace these with your info
+server = 'irc.chat.twitch.tv'
+port = 6667
+nickname = 'your_twitch_username'
+token = 'oauth:your_oauth_token'  # From https://twitchapps.com/tmi/
+channel = '#viydal'  # Include the '#' prefix
+
+# Create and connect socket
+sock = socket.socket()
+sock.connect((server, port))
+
+# Send authentication and join channel
+sock.send(f"PASS {token}\n".encode('utf-8'))
+sock.send(f"NICK {nickname}\n".encode('utf-8'))
+sock.send(f"JOIN {channel}\n".encode('utf-8'))
+
+# Listen to chat
+while True:
+    resp = sock.recv(2048).decode('utf-8')
     
-  pyautogui.click()
+    if resp.startswith('PING'):
+        # Respond to keepalive PINGs
+        sock.send("PONG :tmi.twitch.tv\r\n".encode('utf-8'))
+    else:
+        print(resp)
